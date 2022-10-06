01df06b5336ac540ea226a354357ba8e1d6e42d8
AndreSantos86 <andresantos86@gmail.com>
2022-10-06 17:32:27 -0300

    061022

--- src/pages/Cardapio/Cardapio.tsx
+++ src/pages/Cardapio/Cardapio.tsx
@@ -1,6 +1,10 @@
 import styles from './Cardapio.module.scss'
 import {ReactComponent as LogoAluroni} from  'assets/logoAluroni.svg'
+import Buscador from './Buscador/Buscador'
+import { useState } from 'react'
+import Filtros from './Filtros/Filtros'
 export default function Cardapio() {
+    const [busca, setBusca] = useState('')
     return (
         <main>
             <nav className={styles.menu}>
@@ -11,6 +15,16 @@ export default function Cardapio() {
                     Casa da Massa
                 </div>
             </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                <Buscador
+                    busca={busca}
+                    setBusca={setBusca}
+                />
+                <div className={styles.__filtros}>
+                    <Filtros/>
+                </div>
+            </section>
         </main>
     )
 }
