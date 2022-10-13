a2d4cbd9729a5bb4f2b60ad2896e0e60cb5abdfc
AndreSantos86 <andresantos86@gmail.com>
2022-10-13 18:20:10 -0300

    131022

--- src/pages/Cardapio/Itens/Itens.tsx
+++ src/pages/Cardapio/Itens/Itens.tsx
@@ -1,11 +1,47 @@
 import Item from './Item/Item'
 import cardapio from './Itens.json'
 import styles from './Itens.module.scss'
+import { useState, useEffect } from "react";
 
-export default function Itens(){
+interface ItensProps{
+    busca: string
+    filtro: number | null
+    ordenador: string
+}
+export default function Itens(props: ItensProps){
+    const [lista, setLista] = useState(cardapio)
+    const {busca , filtro, ordenador } = props
+
+    function testeBusca(title: string){
+        const regex = new RegExp(busca, 'i')
+        return regex.test(title)
+    }
+
+    function testaFiltro(id: number){
+        if(filtro !== null) return filtro === id
+        return true;
+    }
+
+    function ordenar(novaLista: typeof cardapio){
+        switch(ordenador){
+            case 'porcao':
+                return novaLista.sort((a,b) => (a.size > b.size ? 1: -1));
+            case "qtd_pessoas":
+                return novaLista.sort((a,b) => (a.serving > b.serving ? 1: -1))
+            case "preco":
+                return novaLista.sort((a,b) => (a.price > b.price ? 1: -1))
+            default:
+                return novaLista
+        }
+    }
+
+    useEffect(()=>{
+        const novaLista = cardapio.filter(item=>testeBusca(item.title) && testaFiltro(item.category.id));
+        setLista(ordenar(novaLista))
+    },[busca, filtro, ordenador])
     return(
         <div className={styles.itens}>
-            {cardapio.map(item =>(
+            {lista.map(item =>(
                 <Item key={item.id}
                 {...item}/>
             ))}
