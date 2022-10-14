e64a11aa361cedca3999d4199cb1d4a8843037c4
AndreSantos86 <andresantos86@gmail.com>
2022-10-14 17:03:28 -0300

    141022

--- src/pages/Cardapio/Buscador/Buscador.tsx
+++ src/pages/Cardapio/Buscador/Buscador.tsx
@@ -1,6 +1,6 @@
-import React from "react"
-import styles from "./Buscador.module.scss";
-import {CgSearch} from "react-icons/cg"
+import React from 'react';
+import styles from './Buscador.module.scss';
+import {CgSearch} from 'react-icons/cg';
 
 interface BuscadorProps{
     busca: string,
@@ -9,14 +9,14 @@ interface BuscadorProps{
 }
 
 export default function Buscador({busca, setBusca}:BuscadorProps){
-    return <div className={styles.buscador}>
-        <input value={busca}
-            onChange={(evento)=> setBusca(evento.target.value)}
-            placeholder="Buscar"
-        /> 
-        <CgSearch
-            size={20}
-            color="#4C4D5E"
-        />
-    </div>
+  return <div className={styles.buscador}>
+    <input value={busca}
+      onChange={(evento)=> setBusca(evento.target.value)}
+      placeholder="Buscar"
+    /> 
+    <CgSearch
+      size={20}
+      color="#4C4D5E"
+    />
+  </div>;
 }
--- src/pages/Cardapio/Cardapio.tsx
+++ src/pages/Cardapio/Cardapio.tsx
@@ -1,38 +1,38 @@
-import styles from './Cardapio.module.scss'
-import {ReactComponent as LogoAluroni} from  'assets/logoAluroni.svg'
-import Buscador from './Buscador/Buscador'
-import { useState } from 'react'
-import Filtros from './Filtros/Filtros'
-import Ordenador from './Ordenador/Ordenador'
-import Itens from './Itens/Itens'
+import styles from './Cardapio.module.scss';
+import { ReactComponent as LogoAluroni } from 'assets/logoAluroni.svg';
+import Buscador from './Buscador/Buscador';
+import { useState } from 'react';
+import Filtros from './Filtros/Filtros';
+import Ordenador from './Ordenador/Ordenador';
+import Itens from './Itens/Itens';
 
 
 export default function Cardapio() {
-    const [busca, setBusca] = useState('')
-    const [filtro , setFiltro] = useState<number| null>(null)
-    const [ordenador , setOrdenador] = useState("")
-    return (
-        <main>
-            <nav className={styles.menu}>
-               <LogoAluroni/>
-            </nav>
-            <header className={styles.header}>
-                <div className={styles.header__text}> 
-                    Casa da Massa
-                </div>
-            </header>
-            <section className={styles.cardapio}>
-                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-                <Buscador
-                    busca={busca}
-                    setBusca={setBusca}
-                />
-                <div className={styles.cardapio__filtros}>
-                    <Filtros filtro={filtro} setFiltro={setFiltro}/>
-                    <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
-                </div>
-                <Itens busca={busca} filtro={filtro} ordenador={ordenador}/>
-            </section>
-        </main>
-    )
+  const [busca, setBusca] = useState('');
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState('');
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <LogoAluroni />
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+          Casa da Massa
+        </div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <Buscador
+          busca={busca}
+          setBusca={setBusca}
+        />
+        <div className={styles.cardapio__filtros}>
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+        </div>
+        <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+      </section>
+    </main>
+  );
 }
