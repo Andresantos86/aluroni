c78ed31d9cc7779caf1e63608080e04e6dbce8b2
AndreSantos86 <andresantos86@gmail.com>
2022-10-22 17:10:49 -0300

    221022

---
+++ src/components/menu/PaginaPadrao/Padrao.tsx
@@ -0,0 +1,18 @@
+import styles from './Padrao.module.scss';
+import { Outlet } from 'react-router-dom';
+import stylesTema from 'styles/Tema.module.scss';
+
+export default function PaginaPadrao() {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+          Casa da Massa
+        </div>
+      </header>
+      <div className={stylesTema.container}>
+        <Outlet />
+      </div>
+    </>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,19 @@
 import Menu from 'components/menu/Menu';
+import PaginaPadrao from 'components/menu/PaginaPadrao/Padrao';
 import Cardapio from 'pages/Cardapio/Cardapio';
 import Inicio from 'pages/Inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import styles from './Routes.modules.scss';
 
 export default function AppRouter() {
   return (
     <main>
       <Router>
-        <Menu />
-        <header className={styles.header}>
-          <div className={styles.header__text}>
-            Casa da Massa
-          </div>
-        </header>
-        <Routes> {/* funciona como switch para na primeira rota que encontra  e no ROUTE e onde ele abre*/}
-          <Route path='/' element={<Inicio />} />
-          <Route path='/cardapio' element={<Cardapio />} />
+        <Menu/>
+        <Routes>{/**Nexted Routes = */}
+          <Route path='/' element={<PaginaPadrao/>}>
+            <Route index element={<Inicio/>}/>
+            <Route path='cardapio' element={<Cardapio/>}/>
+          </Route>
         </Routes>
       </Router>
     </main>
