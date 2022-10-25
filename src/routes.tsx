9e22c2d1bc5f84a346760c2c1066cabbebb1fc1e
AndreSantos86 <andresantos86@gmail.com>
2022-10-25 09:59:54 -0300

    Final

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import Footer from 'components/Footer/Footer';
 import Menu from 'components/menu/Menu';
-import PaginaPadrao from 'components/menu/PaginaPadrao/Padrao';
+import PaginaPadrao from 'components/PaginaPadrao/Padrao';
 import Cardapio from 'pages/Cardapio/Cardapio';
 import Inicio from 'pages/Inicio';
 import NotFound from 'pages/NotFound/NotFound';
@@ -12,17 +12,17 @@ export default function AppRouter() {
   return (
     <main className='container'>
       <Router>
-        <Menu/>
+        <Menu />
         <Routes>{/**Nexted Routes = */}
-          <Route path='/' element={<PaginaPadrao/>}>
-            <Route index element={<Inicio/>}/>
-            <Route path='cardapio' element={<Cardapio/>}/>
-            <Route path='sobre' element={<Sobre/>}/>            
+          <Route path='/' element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
           </Route>
-          <Route path='*' element={<NotFound/>}/>
-          <Route path='prato/:id' element={<Prato/>}/>
+          <Route path='prato/:id' element={<Prato />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </main>
   );
